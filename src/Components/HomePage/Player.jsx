6bc5418ae2df9056c545e9d84e75ce5a0d229c4e
Anusha <anusha19@navgurukul.org>
2021-03-25 18:52:03 +0530

    add background music

--- src/Components/HomePage/Player.jsx
+++ src/Components/HomePage/Player.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useAudio from "./Audio";
+import "./Player.scss";
+import { FaVolumeUp } from "react-icons/fa";
+import { FaVolumeMute } from "react-icons/fa";
 
 function Player({ url, repeat }) {
-  const [playing, toggle] = useAudio(url, repeat);
+  const [playing, toggle, setUrl] = useAudio(url, repeat);
+
+  useEffect(() => {
+    setUrl(url);
+  }, [setUrl, url]);
 
   return (
     <div>
-      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+      <button className="playMusic" onClick={toggle}>
+        {playing ? (
+          <FaVolumeMute fontSize="2rem" />
+        ) : (
+          <FaVolumeUp fontSize="2rem" />
+        )}
+      </button>
     </div>
   );
 }
